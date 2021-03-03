da465011874ff8d4d96605edf28484c774d43ec3
kanso37 <kanso2379@gmail.com>
2021-03-02 23:39:51 -0600

    Homepage up and running with the option to use channel topic as context

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from slack_bolt import App
 import re
+import json
 
 from google_searcher import *
 from topic_extractor import *
 from utility import *
+from homepage_view import *
 
 
 # File containing all keys
@@ -30,6 +32,7 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
+
 # Listens to incoming messages that contain "hello"
 @app.message("Hello")
 def message_hello(message, say):
@@ -54,7 +57,7 @@ def message_hello(message, say):
 # New "what" handler.
 # Always grabs the last word in the sentence.
 @app.message(re.compile('^[Ww]hat\s'))
-def what_handler(message, say):
+def what_handler(message, say, client):
 
     # Word to be searched. Takes last word from input sentence.
     query = message["text"].split()[-1]
@@ -64,6 +67,29 @@ def what_handler(message, say):
     search_string = f'site:en.wikipedia.org {query} {query}'
 
     say(f'You asked about: "{query}." Thinking...')
+
+    # Append channel topic if "use_channel_topics" is True.
+    # https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+
+        if config["use_channel_topics"]:
+            # https://slack.dev/bolt-python/concepts#web-api
+            # https://api.slack.com/methods/conversations.info
+
+            # NB: Because it is Python and not JS (presumably), it is 
+            # conversations_info instead of conversations.info
+            channel_info = client.conversations_info(
+                channel =   message["channel"], # Conversation ID
+                token =     os.environ['SLACK_BOT_TOKEN'] # OAuth token
+            )
+
+            channel_topic = channel_info["channel"]["topic"]["value"]
+            search_string = f'{search_string} {channel_topic}'
+
+            say("Using channel topic for additional context.")
+
+
 
     keywords, topics = definition(message, say)
     
@@ -82,11 +108,65 @@ def what_handler(message, say):
 
     say(link)
 
+# Really good documentation:
+# https://slack.dev/bolt-python/concepts#action-listening
+@app.action("config-checkboxes")
+def config_checkbox_handler(ack, body, client):
+    '''
+    body: Big dictionary containing all the info about the action.
+    ack: You must call ack() in the handler to acknowledge the action.
+    '''
+
+    ack()
+
+    # Will update this to True if the checkbox is actually selected.
+    use_channel_topics = False
+
+    # Array containing all selected checkboxes:
+    selected_checkboxes = body["actions"][0]["selected_options"]
+
+    for checkbox in selected_checkboxes:
+        # This check isn't strictly necessary since we don't have multiple config options,
+        # but if we add more we can build off this. (Add checks for other config options.)
+        if "Use channel topics as search context." in checkbox["text"]["text"]:
+            use_channel_topics = True
+
+    # Update JSON "database":
+    # https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+    config = {}
+    config["use_channel_topics"] = use_channel_topics
+
+    with open('config.json', 'w') as outfile:
+        json.dump(config, outfile)
+    
+    
+
+
+
+
+@app.event("app_home_opened")
+def display_home_tab(client, event, logger):
+
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+        # the user that opened your app's app home
+        user_id=event["user"],
+        # the view object that appears in the app home
+        # See the homepage_view.py file.
+        view=get_homepage_view()
+        )
+  
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+
+
+
 
 # Start your app
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
-
 
 
 
